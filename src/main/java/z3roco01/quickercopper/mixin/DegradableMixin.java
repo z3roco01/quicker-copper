d95c9c772b2a42ee88792a0380bfe833b3d8156c
z3roco01 <zerocool@e.email>
2024-03-26 01:30:02 -0500

    Let blocks in a cube instead increase it

--- src/main/java/z3roco01/quickercopper/mixin/DegradableMixin.java
+++ src/main/java/z3roco01/quickercopper/mixin/DegradableMixin.java
@@ -15,6 +15,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import z3roco01.quickercopper.QuickerCopper;
 
+import javax.swing.*;
 import java.util.Optional;
 
 @Mixin(Degradable.class)
@@ -30,27 +31,36 @@ public interface DegradableMixin<T extends Enum<T>> {
             chanceOxidize += 0.02; // add 2% to the chance to oxidize
 
         int degradedLvlOrdi = this.getDegradationLevel().ordinal();
-        Iterable<BlockPos.Mutable> iterator = BlockPos.iterateInSquare(pos, 1, Direction.EAST, Direction.SOUTH);
-        for(BlockPos.Mutable blockPos : iterator) {
-            Block block = world.getBlockState(blockPos).getBlock();
-            // Check that the block is not this one and is degradable
-            if(blockPos.equals(pos)) continue;
-
-            if(block instanceof Degradable) {
-                Degradable degradable = (Degradable) block;
-
-                int blockDegradeOrdi = degradable.getDegradationLevel().ordinal();
-                if (blockDegradeOrdi > degradedLvlOrdi)
-                    chanceOxidize += 0.008; // add 0.003 to the chance to oxidize per oxidizable block with higher level, can become an increase of 6.4% or 0.064
-            }
-            if(block == Blocks.WATER)
-                chanceOxidize += 0.004; // add 0.0015 to the chance to oxidize per water block around it, can become an increase of 3.2% or 0.032
+        Iterable<BlockPos> iterator = BlockPos.iterateOutwards(pos, 1, 1, 1);
+        for(BlockPos blockPos : iterator) {
+            if(blockPos == pos) continue;
+            chanceOxidize += getIncrease(degradedLvlOrdi, blockPos, world);
         }
 
+
+        QuickerCopper.LOGGER.info(pos + " " + chanceOxidize);
 
         if(random.nextDouble() < chanceOxidize)
             this.getDegradationResult(state).ifPresent(degraded -> world.setBlockState(pos, (BlockState)degraded));
 
         ci.cancel();
     }
+
+    // gets the amount we should increase the chance by for a blockpos
+    private double getIncrease(int degradedLvl, BlockPos blockPos, ServerWorld world) {
+        double increase = 0;
+        Block block = world.getBlockState(blockPos).getBlock();
+        if(block instanceof Degradable) {
+            Degradable degradable = (Degradable) block;
+
+            int blockDegradeOrdi = degradable.getDegradationLevel().ordinal();
+            if (blockDegradeOrdi > degradedLvl)
+                increase += 0.008; // add 0.003 to the chance to oxidize per oxidizable block with higher level, can become an increase of 6.4% or 0.064
+        }
+        if(block == Blocks.WATER)
+            increase += 0.004; // add 0.0015 to the chance to oxidize per water block around it, can become an increase of 3.2% or 0.032
+
+        return increase;
+    }
+
 }
